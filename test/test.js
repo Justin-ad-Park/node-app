da2cffca5215eec727f8a96fa59275b753d4a9e6
justin <justin.ad.park@gmail.com>
2019-03-10 21:54:00 +0900

    Update v0.2

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var request = require('supertest');
 var app = require('../index.js');
 describe('GET /', function() {
-	it('respond with hello world', function(done) {
+	it('respond with hello world v0.2', function(done) {
 
 		//navigate to root and check the response is "hello world"
 		request(app).get('/').expect('hello world v0.2', done);
